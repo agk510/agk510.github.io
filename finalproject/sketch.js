92e33be52690f65002e4110c0535600d98a0f2b1
AGK <agk@talia-coopers-macbook.local>
2015-11-19 11:27:22 -0500

    successfully loaded ttable and parsed edu data into array

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -1,20 +1,27 @@
+var table;
 var edu = []; //, jobcat, salary, salbegin, jobtime, prevexp, race, age, gender = [];
 
 function setup() {
-  loadStrings("http://agk510.github.io/finalproject/libraries/empdat.csv", parseSource); // load source, parse when done
+	createCanvas(windowWidth, windowHeight);
+ 	table = loadTable("http://agk510.github.io/finalproject/libraries/empdat.csv", "csv", "header", parseSource); // load source, parse when done
 }
 
 function draw() {
  
+ellipse(50, 50, 50, 50);
 
+text("Edu = " + edu, 50, 200);
 }
 
-function parseSource(data) {
-	for (var i = 1; i < data.length; i++) {
-		var column = split(data[i], ",");
-		edu[i-1] = column[1];
-	  text("Edu = " + edu);
-		// jobcat[i-1] = column[2];
-		// salary[i-1] = column[3];
-	}
+function parseSource() {
+  for (var i = 0; i < table.getRowCount(); i++){
+    edu[i] = table.get(i,1);
+  }
+// 	for (var i = 1; i < data.length; i++) {
+// 		var column = split(data[i], ",");
+// 		edu[i-1] = column[1];
+// 	  text("Edu = " + edu);
+// 		// jobcat[i-1] = column[2];
+// 		// salary[i-1] = column[3];
+// 	}
 }
