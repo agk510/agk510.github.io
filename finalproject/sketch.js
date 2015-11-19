95e0b618a5a88c209835178027d1ea78e686f164
AGK <agk@taliacosmacbook.home>
2015-11-19 09:06:34 -0500

    loading empdat.csv from server

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -1,5 +1,7 @@
+var edu = []; //, jobcat, salary, salbegin, jobtime, prevexp, race, age, gender = [];
+
 function setup() {
-  
+  loadStrings("http://agk510.github.io/finalproject/libraries/empdat.csv", parseSource); // load source, parse when done
 }
 
 function draw() {
@@ -7,3 +9,12 @@ function draw() {
 
 }
 
+function parseSource(data) {
+	for (var i = 1; i < data.length; i++) {
+		var column = split(data[i], ",");
+		edu[i-1] = column[1];
+	  text("Edu = " + edu);
+		// jobcat[i-1] = column[2];
+		// salary[i-1] = column[3];
+	}
+}
