8832c8ce8bb14ec1df0fa40bf2ea1a83ea011a16
newschool <newschool@d600cart-mac11.tns.newschool.edu>
2015-11-24 14:47:29 -0500

    another update

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -11,7 +11,6 @@ var age = [];
 var gender = [];
 
 var canvas;
-// var graphContext, graphGender, graphRace; // create graphics for each section 
 var sectionHeight = 400;
 
 function preload() {
@@ -46,33 +45,33 @@ function setup() {
 function draw() {
    
     // spaceholder for background context and description
-    //noStroke();
+    // noStroke();
+    var sectionindex = 1;
+
+    var bgcolor = '#e5f0ff';
+    var cat1color = '#80ccff';
+    var cat2color = '#006cff';
+
+    // placeholder for contextual information section
     fill(255);
+    noStroke();
     rect(0, 0, width, sectionHeight);
     fill(100);
     textSize(20);
     text("I am a placeholder for background context and description.", 100, 100); // alternately could create a div for this text 
 
+    // new section for first dataset: overall gender breakdown
+    push();
+    translate(0, sectionHeight*sectionindex);
+    sectionindex++;
+    noStroke();
+    fill(bgcolor);
+    rect(0, 0, width, sectionHeight);
+
     var men = countType(gender, 'Male');
     var women = countType(gender, 'Female');
     var percMen = men / (men + women);
     var percWomen = women / (men + women);
-    var cat1color = 'turquoise';
-    var cat2color = 'blue';
-    
-
-    // new section for first dataset
-    push();
-    translate(0, sectionHeight*1);
-    //noStroke();
-    fill(255);
-    rect(0, 0, width, sectionHeight);
-
-
-    //create buttons to toggle between pie charts and graphs
-    var button1 = createButton('1', 1)
-    button1.position(width/4, sectionHeight-10); // note that button is not included in translate function -- need to reposition under the pie chart
-    // button1.mousePressed(swithGraph);
 
     //draw pie chart to show overall gender breakdown in firm
     noStroke();
@@ -83,6 +82,17 @@ function draw() {
     arc(width/4, sectionHeight/2, sectionHeight/2, sectionHeight/2, -HALF_PI + percWomen*TWO_PI, -HALF_PI + percWomen*TWO_PI + percMen*TWO_PI, PIE);
   	text(nf(percMen*100, 2, 1) + "% Men", width/4, sectionHeight - sectionHeight/4+40); // create div instead
     
+    pop();
+    
+    // temporary section break -- will go away once button toggling is active
+    push();
+    translate(0, sectionHeight*sectionindex);
+    sectionindex++;
+    noStroke();
+    fill(255);
+    rect(0, 0, width, sectionHeight);
+
+
     //calculate average salary for each gender
     var womenSal = [];
     womenSal = getArray(salary, gender, "Female");
@@ -118,40 +128,127 @@ function draw() {
     rect(width-width/4+70, sectionHeight-sectionHeight/4, 50, -map(avgBegM, 0, 50000, 0, sectionHeight/2));
     text("Starting salary (avg): $" + nf(avgBegM, 0, 2), width-width/4, sectionHeight-sectionHeight/4+40);
     
+    //create buttons to toggle between pie charts and graphs
+    // var button1 = createButton("Current Salary", 1);
+    // button1.position(width/4, sectionHeight-10); // note that button is not included in translate function -- need to reposition under the pie chart
+    // // button1.mousePressed(swithGraph);
+    pop();
+    
+    
+    // new section for job categories
+    push();
+    translate(0, sectionHeight*sectionindex);
+    sectionindex++;
+    noStroke();
+    fill(255);
+    rect(0, 0, width, sectionHeight); 
+
+    // calculate percentages of men/women by job category
+    var numWmanagers = countSubtype(gender, 'Female', jobcat, 'Manager');
+    var numMmanagers = countSubtype(gender, 'Male', jobcat, 'Manager');
+    var percMmanagers = numMmanagers / (numWmanagers + numMmanagers);
+    var percWmanagers = numWmanagers / (numWmanagers + numMmanagers);
+
+    var numWclerical = countSubtype(gender, 'Female', jobcat, 'Clerical');
+    var numMclerical = countSubtype(gender, 'Male', jobcat, 'Clerical');
+    var percWclerical = numWclerical / (numWclerical + numMclerical);
+    var percMclerical = numMclerical / (numWclerical + numMclerical);
+ 
+    var numWcustodial = countSubtype(gender, 'Female', jobcat, 'Custodial');
+    var numMcustodial = countSubtype(gender, 'Male', jobcat, 'Custodial');
+    var percWcustodial = numWcustodial / (numWcustodial + numMcustodial);
+    var percMcustodial = numMcustodial / (numWcustodial + numMcustodial);
+
+    // draw pie charts for men/women by job category
+    noStroke();
+    fill(cat1color);
+    arc(width/4, sectionHeight/2, sectionHeight/2, sectionHeight/2, -HALF_PI, -HALF_PI + percWmanagers*TWO_PI, PIE);
+    text(nf(percWmanagers*100, 2, 1) + "% Female Managers", width/4, sectionHeight - sectionHeight/4 + 20);
+    fill(cat2color);
+    arc(width/4, sectionHeight/2, sectionHeight/2, sectionHeight/2, -HALF_PI + percWmanagers*TWO_PI, -HALF_PI + percWmanagers*TWO_PI + percMmanagers*TWO_PI, PIE);
+    text(nf(percMmanagers*100, 2, 1) + "% Male Managers", width/4, sectionHeight - sectionHeight/4+40); // create div instead
+    
+    fill(cat1color);
+    arc(2*width/4, sectionHeight/2, sectionHeight/2, sectionHeight/2, -HALF_PI, -HALF_PI + percWclerical*TWO_PI, PIE);
+    text(nf(percWclerical*100, 2, 1) + "% Female Clericals", 2*width/4, sectionHeight - sectionHeight/4 + 20);
+    fill(cat2color);
+    arc(2*width/4, sectionHeight/2, sectionHeight/2, sectionHeight/2, -HALF_PI + percWclerical*TWO_PI, -HALF_PI + percWclerical*TWO_PI + percMclerical*TWO_PI, PIE);
+    text(nf(percMclerical*100, 2, 1) + "% Male Clericals", 2*width/4, sectionHeight - sectionHeight/4+40); // create div instead
+
+    fill(cat1color);
+    arc(3*width/4, sectionHeight/2, sectionHeight/2, sectionHeight/2, -HALF_PI, -HALF_PI + percWcustodial*TWO_PI, PIE);
+    text(nf(percWcustodial*100, 2, 1) + "% Female Custodials", 3*width/4, sectionHeight - sectionHeight/4 + 20);
+    fill(cat2color);
+    arc(3*width/4, sectionHeight/2, sectionHeight/2, sectionHeight/2, -HALF_PI + percWcustodial*TWO_PI, -HALF_PI + percWcustodial*TWO_PI + percMcustodial*TWO_PI, PIE);
+    text(nf(percMcustodial*100, 2, 1) + "% Male Custodials", 3*width/4, sectionHeight - sectionHeight/4+40); // create div instead
+
     pop();
 
-    
-    // create next section
+    // temporary section break -- take out when buttons become active
     push();
-    translate(0, sectionHeight*2);
+    translate(0, sectionHeight*sectionindex);
+    sectionindex++;
     //noStroke();
-    fill(255);
-    rect(0, 0, width, sectionHeight*2); // resize height to sectionHeight x1 once the buttons are functional
 
     //calculate avg salaries for men and women in different job categories
-    var Wmanagers = new Array(getArray(salary, gender, "Female", jobcat, "Manager"));
+    var Wmanagers = [];
+    Wmanagers = getArray(salary, gender, "Female", jobcat, "Manager");
     var avgWmanager = average(Wmanagers); // average salary for female managers
 
-    var Mmanagers = new Array(getArray(salary, gender, "Male", jobcat, "Manager"));
+    var Mmanagers = [];
+    Mmanagers = getArray(salary, gender, "Male", jobcat, "Manager");
     var avgMmanager = average(Mmanagers); // average salary for male managers
 
-    var Wclericals = new Array(getArray(salary, gender, "Female", jobcat, "Clerical"));
+    var Wclericals = [];
+    Wclericals = getArray(salary, gender, "Female", jobcat, "Clerical");
     var avgWclerical = average(Wclericals); // average salaary for female clericals
 
-    var Mclericals = new Array(getArray(salary, gender, "Female", jobcat, "Clerical"));
+    var Mclericals = [];
+    Mclericals = getArray(salary, gender, "Female", jobcat, "Clerical");
     var avgMclerical = average(Mclericals); // average salary for male clericals
 
-    var Wcustodials = new Array(getArray(salary, gender, "Female", jobcat, "Custodial"));
+    var Wcustodials = [];
+    Wcustodials = getArray(salary, gender, "Female", jobcat, "Custodial");
     var avgWcustodial = average(Wcustodials); // average salary for female custodials
 
-    var Mcustodials = new Array(getArray(salary, gender, "Male", jobcat, "Custodial"));
+    var Mcustodials = [];
+    Mcustodials = getArray(salary, gender, "Male", jobcat, "Custodial");
     var avgMcustodial = average(Mcustodials); // average salary for male custodials
 
+    
     // draw bar graphs for men/women salaries by job category
+    noStroke();
+    fill(cat1color);
+    rect(width/4, sectionHeight-sectionHeight/4, 50, -map(avgWmanager, 0, 50000, 0, sectionHeight/2));    
+    text("Manager salary (avg): $" + nf(avgWmanager, 0, 2), width/4, sectionHeight-sectionHeight/4+20);
+    fill(cat2color);
+    rect(width/4 + 70, sectionHeight-sectionHeight/4, 50, -map(avgMmanager, 0, 50000, 0, sectionHeight/2));
+    text("Manager salary (avg): $" + nf(avgMmanager, 0, 2), width/4, sectionHeight-sectionHeight/4+40);
 
+    fill(cat1color);
+    rect(2*width/4, sectionHeight-sectionHeight/4, 50, -map(avgWclerical, 0, 50000, 0, sectionHeight/2));    
+    text("Clerical salary (avg): $" + nf(avgWclerical, 0, 2), 2*width/4, sectionHeight-sectionHeight/4+20);
+    fill(cat2color);
+    rect(2*width/4 + 70, sectionHeight-sectionHeight/4, 50, -map(avgMclerical, 0, 50000, 0, sectionHeight/2));
+    text("Clerical salary (avg): $" + nf(avgMclerical, 0, 2), 2*width/4, sectionHeight-sectionHeight/4+40);
+
+    fill(cat1color);
+    rect(3*width/4, sectionHeight-sectionHeight/4, 50, -map(avgWcustodial, 0, 50000, 0, sectionHeight/2));    
+    text("Custodial salary (avg): $" + nf(avgWcustodial, 0, 2), 3*width/4, sectionHeight-sectionHeight/4+20);
+    fill(cat2color);
+    rect(3*width/4 + 70, sectionHeight-sectionHeight/4, 50, -map(avgMcustodial, 0, 50000, 0, sectionHeight/2));
+    text("Custodial salary (avg): $" + nf(avgMcustodial, 0, 2), 3*width/4, sectionHeight-sectionHeight/4+40);
 
     pop();
 
+    //new section for race
+    push();
+    translate(0, sectionHeight*sectionindex);
+    sectionindex++;
+    //noStroke();
+    fill(255);
+    rect(0, 0, width, sectionHeight);
+    
     //calculate avg salaries for white vs. non-white men and women
     var PoCwomen = [];
     PoCwomen = getArray(salary, race, "Yes", gender, "Female");
@@ -188,6 +285,14 @@ function draw() {
 
     pop();
 
+
+    push();
+    translate(0, sectionHeight*sectionindex);
+    sectionindex++;
+    //noStroke();
+    fill(255);
+    rect(0, 0, width, sectionHeight);
+    
 
 }
 
@@ -227,8 +332,7 @@ function countType (array, type) {  // returns the number of occurrences of type
 function countSubtype (array, type, subarray, subtype) { // returns the number of occurrences of subtype that correspond with type
     var subcount = 0;
     for (var i = 0; i < array.length; i++)
-        if (array[i] === type)
-            if (subarray[i] === subtype)
+        if (array[i] === type && subarray[i] === subtype)
                 subcount++;
 
     return subcount;
